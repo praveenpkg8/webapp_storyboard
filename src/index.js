0bce883fb1b31383efb63823e8debee0b7770669
praveen <praveenpkg8@gmail.com>
2019-07-28 18:30:22 +0530

    created: index component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App'
+
+const Index = () => {
+    return <div><App /></div>
+}
+
+ReactDom.render(<Index />, document.getElementById('index'));
