1ec6c86a8eff2b6102a867a4b72533c29e4e079d
praveen <praveenpkg8@gmail.com>
2019-07-28 18:29:17 +0530

    created: app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+
+import RouteConfig from './RouteConfig/RouteConfig'
+
+function App() {
+    return (
+        <>
+        <RouteConfig />
+        </>
+    )
+}
+
+export default App;
