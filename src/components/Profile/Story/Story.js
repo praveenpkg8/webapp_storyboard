9027afe535ae0f6b7aa2976884ef6f318baa69da
praveen <praveenpkg8@gmail.com>
2019-07-28 18:35:18 +0530

    created: story component

---
+++ src/components/Profile/Story/Story.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import StoryItem from './StoryItem';
+
+
+const URL = '/api/story';
+
+export default class Story extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stories: [],
+            loadStories: false,
+            user: props.user,
+            story: "",
+        }
+    }
+
+    handleChange = (event) => {
+        var target = event.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({
+            [name]: value
+          });
+
+    }
+
+    componentWillMount() {
+        this.getAllStories ()
+    }
+
+    reRender = (data) => {
+        let story = this.state.stories
+        let list = [data.message,...story]
+        this.setState({
+            stories: list
+        })
+    }
+
+    getAllStories = async() => {
+        const story = await fetch(URL, {credentials: 'include'})
+        const stories = await story.json()
+        this.setState({ 
+            story_detials: stories,
+            stories: stories.message,
+            loadStories: true
+        })
+    }
+
+    getNextStory = async() => {
+        let story_detials = this.state.story_detials
+        let url = '/api/story?next_cursor=' + story_detials.next_cursor
+        const response = await fetch(url, {credentials: 'include'})
+        const stories = await response.json()
+        let story = [...this.state.stories, ...stories.message]
+        this.setState({
+            story_detials: stories,
+            stories: story
+        })
+    }
+
+    render () {
+        if (this.state.loadStories){
+            let stories = this.state.stories;
+            let story_detials = this.state.story_detials;
+            let storyItem = stories.map((story, index) => <StoryItem key={story.story_id} story={story} user={this.state.user} />);
+            return (
+                <>
+                hi
+                {/* <UpdateStory
+                    user_detials={this.state.user} 
+                    render={this.reRender}
+                />
+                {storyItem}
+                { story_detials.more ? <button type="button" onClick={this.getNextStory} className="btn btn-link">Load Stories....</button> : <></>} */}
+                </>
+            )
+        }
+        return <></>
+        
+    }
+}
