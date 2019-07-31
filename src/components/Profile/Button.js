e1fd4647f04923df5f2218b1a719d380a03850ff
praveen <praveenpkg8@gmail.com>
2019-07-31 10:14:09 +0530

    created: button component

---
+++ src/components/Profile/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logout from '../Authentication/Logout';
+import InviteFriends from './InviteFriends';
+
+const Button = () => {
+    return (
+        <>
+        <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+            <div className="btn-group" role="group" aria-label="First group">
+            <Logout />
+            </div>
+            <div className="input-group">
+            <InviteFriends />
+            </div>
+            </div>
+        
+
+        </>
+    )
+}
+
+export default Button;
