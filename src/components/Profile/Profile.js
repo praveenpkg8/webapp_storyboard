866491ffc56a96b34a33fa2361e85fd341cc7110
praveen <praveenpkg8@gmail.com>
2019-09-03 11:29:39 +0530

    modified according to eslint structure

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
 import Cookies from 'js-cookie';
@@ -12,99 +13,99 @@ const URL = '/api/auth';
 function Loading() {
     return (
         <>
-        <div className="spinner-border text-primary" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-secondary" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-success" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-danger" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-warning" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
-        <div className="spinner-border text-info" role="status">
-        <span className="sr-only">Loading...</span>
-        </div>
+            <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-secondary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-success" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-danger" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-warning" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <div className="spinner-border text-info" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
         </>
     );
-    
 }
 
 
-class User extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            user_detials: props.user,
-        }
-        
-    }
-
-    render() {
-        return (
-            <>
-            <h1>{this.state.user_detials.name}</h1>
+function User(props) {
+    const { userDetials } = props;
+    return (
+        <>
+            <h1>{userDetials.name}</h1>
             <h2>Welcome Aboard ....!!!</h2>
-            </>
-        )
-    }
+        </>
+    );
 }
+
+User.defaultProps = {
+    userDetials: {},
+};
+
+User.propTypes = {
+    userDetials: PropTypes.objectOf(PropTypes.object),
+};
+
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user : '',
+            user: '',
             loaded: false,
-            story: '', 
-            render: true,
-            status: 0
-
-        }
+            status: 0,
+        };
     }
 
-    profile = async() => {
-        const data = await fetch(URL,{credentials: 'include'});
-        const user = await data.json();
-        const status = await data.status;
-        this.setState({
-            user : user.message,
-            loaded: true,
-            status: status
-        })
-    }
-    
     componentWillMount() {
         this.profile();
     }
 
+    profile = async () => {
+        try {
+            const data = await fetch(URL, { credentials: 'include' });
+            const status = await data.status;
+            const user = await data.json();
+            this.setState({
+                user: user.message,
+                loaded: true,
+                status,
+            });
+        } catch {
+            // Do nothing
+        }
+    }
+
     render() {
-        if (Cookies.get('session') !== undefined && this.state.status === 200) {
+        const { status, user, loaded } = this.state;
+        if (Cookies.get('session') !== undefined && status === 200) {
             return (
-                
-                this.state.loaded ? 
-                <>
-                <div className='container'>
-                <User user={this.state.user} />
-                <Button />
-                <Story user={this.state.user} />
-                </div>
-                </>
-                :
-                <h1>Loading.........</h1>
-                )
-            }
-            else if (Cookies.get('session') === undefined || this.state.user !== '' || this.state.status === 400 ) {
-                return <Redirect to='/landing-page' />
-            }
-            else{
-                return <Loading />
-            }   
-    
+
+                loaded
+                    ? (
+                        <>
+                            <div className="container">
+                                <User userDetials={user} />
+                                <Button />
+                                <Story user={user} />
+                            </div>
+                        </>
+                    )
+                    : <h1>Loading.........</h1>
+            );
+        }
+        if (Cookies.get('session') === undefined || user !== '' || status === 400) {
+            return <Redirect to="/landing-page" />;
+        }
+
+        return <Loading />;
     }
 }
