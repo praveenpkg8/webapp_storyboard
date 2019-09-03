3c46c79a3e5a2cca4bee22ef3b7c86f69ae4a03f
praveen <praveenpkg8@gmail.com>
2019-09-03 11:28:59 +0530

    modifed according to eslint structure

--- src/components/Profile/InviteFriends.js
+++ src/components/Profile/InviteFriends.js
@@ -1,12 +1,10 @@
 import React from 'react';
-const URL = 'http://localhost:8080';
-const ProductionURL = 'https://full-services.appspot.com'
-const InviteFriends = () => {
-    return (
-        <>
-        <a href={ProductionURL +'/api/contact/oauth'} className="btn btn-outline-success">Invite friends</a>
-        </>
-    )
-}
+
+const ProductionURL = 'https://full-services.appspot.com';
+const InviteFriends = () => (
+    <>
+        <a href={`${ProductionURL}/api/contact/oauth`} className="btn btn-outline-success">Invite friends</a>
+    </>
+);
 
 export default InviteFriends;
