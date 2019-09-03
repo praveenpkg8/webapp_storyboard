9c5445c4a1404c98906416b9bad11879e5727d34
praveen <praveenpkg8@gmail.com>
2019-09-03 11:27:46 +0530

    modified according to eslint structure

--- src/components/Authentication/Signin.js
+++ src/components/Authentication/Signin.js
@@ -1,104 +1,139 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import GoogleAuth from './GoogleAuth';
+import Cookies from 'js-cookie';
 
 
-const URL = "/api/auth/signin";
+const URL = '/api/auth/signin';
+
 
 export default class Signin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mail: "",
-            password: "",
+            mail: '',
+            password: '',
             redirect: false,
-        }
+            errorMessage: false,
+            error: '',
+            reset: false,
+        };
     }
 
     handleChange = (event) => {
-        var target = event.target;
-        const name = target.name;
-        const value = target.value;
+        const { target } = event;
+        const { name } = target;
+        const { value } = target;
         this.setState({
-            [name]: value
-          });
+            [name]: value,
+        });
     }
 
-    onSignin = async() => {
+    hideErrorMessage = () => {
+        this.setState({
+            errorMessage: false,
+        });
+    }
+
+    onReset = () => {
+        this.setState({
+            reset: true,
+        });
+    }
+
+    onSignin = async () => {
+        const { mail, password } = this.state;
         const setting = {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
-                mail: this.state.mail,
-                password: this.state.password
-              })
-        }
-        const data = await fetch(URL, setting)
-        .then((response) => response.json().then(data => ({
-            data: data,
-            status: response.status
-        })))
+                mail,
+                password,
+            }),
+        };
+        const response = await fetch(URL, setting);
+        const data = await response.json();
+        console.log(data)
         this.setState({
             mail: '',
-            password: ''
-        })
+            password: '',
+        });
 
-        if(data.status === 200) {
-            document.cookie = "session=" + data.data.session;
-            this.setState({ redirect: true})
+        if (response.status === 200) {
+            Cookies.set('session', data.session);
+            this.setState({ redirect: true });
+        } else if (response.status === 400) {
+            this.setState({
+                errorMessage: true,
+                error: data.message,
+            });
         }
-        else {
-            return <Redirect to='/signin' />
-        }
-
-
+        return null;
     }
 
-    render () {
-        if (this.state.redirect){
-            return <Redirect to='/profile'/>;
+    render() {
+        const {
+            redirect, mail, password, errorMessage, error, reset,
+        } = this.state;
+        if (redirect) {
+            return <Redirect to="/" />;
+        }
+        if (reset) {
+            return <Redirect to="/reset" />;
         }
         return (
             <>
-            <div className="card w-50">
-                <h5 className="card-header">SignIn</h5>
-                <div className="card-body">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text">Mail Id</span>
+                { errorMessage ? (
+                    <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Warning!</strong>
+                        {' '}
+                        {error}
+                        <button type="button" className="close" data-dismiss="alert" onClick={this.hideErrorMessage} aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <input
-                    name="mail" 
-                    type="mail"
-                    className='form-control'
-                    value={this.state.mail}
-                    onChange={this.handleChange} 
-                    />
-                </div>
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text">Password</span>
+                ) : <></>}
+                <div className="card w-50">
+                    <h5 className="card-header">SignIn</h5>
+                    <div className="card-body">
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">Mail Id</span>
+                            </div>
+                            <input
+                                name="mail"
+                                type="mail"
+                                className="form-control"
+                                value={mail}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">Password</span>
+                            </div>
+                            <input
+                                name="password"
+                                type="password"
+                                className="form-control"
+                                value={password}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-outline-info" onClick={this.onSignin}>Login</button>
+                        <button type="button" onClick={this.onReset} className="btn btn-link">Forgot Password</button>
+
+
+                        <h4>
+                            Login with
+                            <GoogleAuth />
+                        </h4>
                     </div>
-                    <input 
-                    name="password"
-                    type="password"
-                    className='form-control'
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    />
                 </div>
-
-                    <button className='btn btn-outline-info' onClick={this.onSignin}>Login</button>
-                    <h4>Login with <GoogleAuth /></h4>   
-                </div>
-            </div>
-            
-            
-
             </>
-        )
+        );
     }
 }
