66490ef36fef15e489df032f1c8fbcec0847c720
praveen <praveenpkg8@gmail.com>
2019-09-03 11:25:16 +0530

    modifed landing page according to reset structure

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Signin from './Authentication/Signin';
 import Signup from './Authentication/Signup';
 
@@ -6,50 +6,80 @@ export default class LandingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            render: true
-        }
+            render: true,
+            signupMessage: '',
+            displayMessage: false,
+        };
+    }
+
+    componentWillMount() {
     }
 
     handleSignin = () => {
-        let val = this.state.render
-        if (!val) {
+        const { render } = this.state;
+        if (!render) {
             this.setState({
-                render: !val
-            })
+                render: !render,
+            });
         }
-        
     }
-    componentWillMount(){
-    }
+
     handleSignup = () => {
-        let val = this.state.render
-        if (val) {
+        const { render } = this.state;
+        if (render) {
             this.setState({
-                render: !val
-            })
+                render: !render,
+            });
         }
-        
+    }
+
+    onSignup = (data) => {
+        const { render } = this.state;
+        if (!render) {
+            this.setState({
+                render: !render,
+                signupMessage: data.message,
+                displayMessage: true,
+            });
+        }
+    }
+
+    hideMessage = () => {
+        this.setState({
+            displayMessage: false,
+        });
     }
 
     render() {
+        const { render, displayMessage, signupMessage } = this.state;
         return (
             <>
-            <div className='container'>
-            <br/>
-                <br/>
+                <div className="container">
+                    { displayMessage ? (
+                        <div className="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Welcome !</strong>
+                            {' '}
+                            {signupMessage}
+                            <button type="button" className="close" data-dismiss="alert" onClick={this.hideMessage} aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    ) : <></>}
+                    <br />
+                    <br />
 
-                <div className="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" onClick={this.handleSignin} className="btn btn-secondary">SignIn</button>
-                    <button type="button" onClick={this.handleSignup} className="btn btn-secondary">SignUp</button>
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" onClick={this.handleSignin} className="btn btn-secondary">SignIn</button>
+                        <button type="button" onClick={this.handleSignup} className="btn btn-secondary">SignUp</button>
+                    </div>
+                    <br />
+                    <br />
+
+                    {render ? <Signin /> : <Signup onSignup={this.onSignup} />}
+
                 </div>
-                <br/>
-                <br/>
-
-                {this.state.render?<Signin /> :<Signup />} 
-
-            </div>
 
             </>
-        )
+        );
     }
 }
